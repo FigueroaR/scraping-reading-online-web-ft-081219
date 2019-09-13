819a718b6b381a5e4ba85289d778af3efe8901f7
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-09-13 18:07:35 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 
-doc = Nokogiri::HTML(open("http://flatironschool.com/"))
-doc.css(".site-header__hero__headline")
+html = open("http://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+ 
+doc.css(".tout__label.heading.heading--level-4")
